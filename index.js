b56a1731d709d9252c374b6bb51cf03023573d13
thatOneJon <83345498+ThatOneJon@users.noreply.github.com>
2023-02-12 22:32:14 +0100

    jwt

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ app.post("/api/login", async (req, res, next) => {
         return res
         .cookie("access_token", token, {
           httpOnly: false,
-          sameSite:None,
+          SameSite: "None",
           secure: true
         })
 
