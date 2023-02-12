61b6a9159d2e4d71862c93153dc40d29177a0f05
thatOneJon <83345498+ThatOneJon@users.noreply.github.com>
2023-02-12 22:19:02 +0100

    cors

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ app.post("/api/login", async (req, res, next) => {
       }
 
     }catch(error){
-      res.json({err:JSON.stringify(error)})
+      res.json({err:"Something went wrong!"})
     }
     return next()
   })
